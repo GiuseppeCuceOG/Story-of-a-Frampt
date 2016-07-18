1e4372a3853ae2c5dc97a3a12eb064dbccff7725
GiuseppeCuce <giuseppecuce.og@gmail.com>
2016-07-18 15:47:27 +0300

    js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ alert("Tuppy Game");
 var main = function () {
 	$("h1").text("GOOO!!!!");
 	$(this).keydown( function(key) {
-		switch(parseInt(key.which,10)) {
+		switch(key.which) {
 			case 37:
 				$('img').animate({left: "-=15px"}, 20);
 				break;
