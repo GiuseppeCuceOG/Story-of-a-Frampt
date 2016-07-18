6bae38f8430148e389e88e8db5d4005f558d70e0
GiuseppeCuce <giuseppecuce.og@gmail.com>
2016-07-18 13:43:16 +0300

    main

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 alert("Tuppy Game");
-var x = prompt("Who you are?").toUpperCase();
-if (x === "FRAMPT"){
-	console.log("Welcome, Guiltier's Seeker..., We knew you would been back again");
-	confirm("you were the real chosen one, the only one who could link the fire to the darkness, that was the only way to maintain the flame eternally");
 	$(document).ready(function() {
-		$("h1").text("Welcome, Guiltier's Seeker");
+		$("h1").text("GOOO!!!!");
 	});
-}
-else {
-	console.log("Aha! another visitor we have, welcome" + " " + x);
-	confirm("Eheh welcome young traveler, I am Irishilla");
-	$(document).ready( function() {
-		$("h1").text("Welcome home young undead this is Looren Valley");
-	});
-};
