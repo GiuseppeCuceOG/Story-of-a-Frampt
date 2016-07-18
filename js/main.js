bf091766fa77bf956b20ec5273c6d6a2d6b2ebf7
GiuseppeCuce <giuseppecuce.og@gmail.com>
2016-07-18 14:32:06 +0300

    js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ alert("Tuppy Game");
 
 var main = function () {
 	$("h1").text("GOOO!!!!");
+	$(document).keydown( function(key) {
+		switch(parseInt(key.which,10)) {
+			case 37:
+				$('img').animate({left: "-=15px"}, 20);
+				break;
+		
+		}
+	});
 }
 
 $(document).ready(main);
