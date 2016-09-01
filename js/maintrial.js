bd170bcba2ae03fe634e451e9eb42f76d0e88779
GiuseppeCuce <giuseppecuce.og@gmail.com>
2016-09-01 17:00:05 +0300

    oggettodisplayed

--- js/maintrial.js
+++ js/maintrial.js
@@ -4,7 +4,7 @@ var Panther = {
 	name: "Panther",
 	health: 10,
 	energy: 5,
-	attack: 0
+	attack: 0,
 };
 
 var Bear = {
@@ -17,12 +17,16 @@ var Bear = {
 var main = function() {
 
 		$('#pantera').click(function() {
+			document.getElementById("name").innerHTML = Panther.name= prompt("What is the name?");
 			$('#orso').fadeOut(1000);
 			$('#sceltaciboP').fadeIn(1000, function() {
 				$('#deer').click(function() {
 					Panther.health = Panther.health + 10;
 					Panther.energy = Panther.energy + 5;
 					Panther.attack = Panther.attack + 3;
+					document.getElementById("health").innerHTML = Panther.health;
+					document.getElementById("energy").innerHTML = Panther.energy;
+					document.getElementById("attack").innerHTML = Panther.attack;
 					console.log(Panther);
 					$('#mushrooms').fadeOut(1000);
 					$('#Cervovivo').fadeIn(1000);
@@ -35,25 +39,28 @@ var main = function() {
 								Panther.health = Panther.health + 20;
 								Panther.energy = Panther.energy + 10;
 								Panther.attack = Panther.attack + 2;
+								document.getElementById("health").innerHTML = Panther.health;
+								document.getElementById("energy").innerHTML = Panther.energy;
+								document.getElementById("attack").innerHTML = Panther.attack;
 								console.log("Prey Caught! Panther is getting stronger! Well made!");
-								console.log(Panther);
-								console.log(escapeDeer);
-								$('#qui').append(Panther);
 							}
 							else { //missed the prey
 								Panther.health = Panther.health - 4;
 								Panther.energy = Panther.energy - 3;
 								Panther.attack = Panther.attack - 1;
+								document.getElementById("health").innerHTML = Panther.health;
+								document.getElementById("energy").innerHTML = Panther.energy;
+								document.getElementById("attack").innerHTML = Panther.attack;
 								console.log("Panther missed the prey and lost energy, Panther is weak!");
-								console.log(Panther);
-								console.log(escapeDeer);
 							}
 						}
 						else if(choice === 'SNEAKY') {
 							Panther.health = Panther.health + 15;
 							Panther.energy = Panther.energy + 5;
 							Panther.attack = Panther.attack + 1;
-							console.log(Panther);
+							document.getElementById("health").innerHTML = Panther.health;
+							document.getElementById("energy").innerHTML = Panther.energy;
+							document.getElementById("attack").innerHTML = Panther.attack;
 							console.log("Uhh that's clever!")
 						}
 						return false;
@@ -72,6 +79,7 @@ var main = function() {
 							Panther.energy = Panther.energy - 3;
 							$('#goat, #vegetables').fadeOut(1000);
 							$('#risposte').append('<p>That was not a nice choice since mushrooms did not give you attack skills, Panther is terribly weak</p>');
+							$('#risposte').fadeIn(1000);
 							console.log(Panther);
 							return false;
 						});
@@ -81,6 +89,7 @@ var main = function() {
 							Panther.attack = Panther.attack + 2;
 							$('#deerAlive, #vegetables').fadeOut(1000);
 							$('#risposte').append('<p>Wise choice! Why risking to catch an alive prey when you can get a ready one? Panther is getting better</p>');
+							$('#risposte').fadeIn(1000);
 							console.log(Panther);
 							return false;
 						});
@@ -89,6 +98,7 @@ var main = function() {
 							Panther.energy = Panther.energy + 11;
 							$('#deerAlive, #goat').fadeOut(1000);
 							$('#risposte').append('<p>Seems Panther forgot its instinct a bit, Panther is also getting fatter but it looks like quite happy</p>');
+							$('#risposte').fadeIn(1000);
 							console.log(Panther);
 							return false;
 						});
@@ -96,9 +106,7 @@ var main = function() {
 					return false;
 				});
 			});
-			
 		});
-
 }
 
 $(document).ready(main);
